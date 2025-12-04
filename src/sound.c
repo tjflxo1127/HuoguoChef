e82d7462455b47d5a25b1011db3ac0b4e44ee173
xstarisborn <aidan15@naver.com>
2025-12-04 19:10:07 +0900

    Add bomb sound effect and update slash sound
    Added bomb sound effect and updated slash sound file.

--- src/sound.c
+++ src/sound.c
@@ -1,25 +1,34 @@
 #include "sound.h"
 #include <stdio.h>
 
-// SDL_mixer 효과음 핸들
+// 효과음 핸들
 static Mix_Chunk *slash_sound = NULL;
+static Mix_Chunk *bomb_sound = NULL;
 
 void InitSound(void) {
-    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 256) < 0) {
+    if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 512) < 0) {
         printf("SDL_mixer init error: %s\n", Mix_GetError());
     }
 }
 
 void LoadEffects(void) {
-    slash_sound = Mix_LoadWAV("./sound/slice.wav"); 
+    slash_sound = Mix_LoadWAV("./sound/slash.wav");
     if (!slash_sound) printf("Slash sound load error: %s\n", Mix_GetError());
+
+    bomb_sound = Mix_LoadWAV("./sound/bomb.wav");
+    if (!bomb_sound) printf("Bomb sound load error: %s\n", Mix_GetError());
 }
 
 void PlaySlashSound(void) {
     if (slash_sound) Mix_PlayChannel(-1, slash_sound, 0);
 }
 
+void PlayBombSound(void) {
+    if (bomb_sound) Mix_PlayChannel(-1, bomb_sound, 0);
+}
+
 void CleanupSound(void) {
     if (slash_sound) Mix_FreeChunk(slash_sound);
+    if (bomb_sound) Mix_FreeChunk(bomb_sound);
     Mix_CloseAudio();
 }
