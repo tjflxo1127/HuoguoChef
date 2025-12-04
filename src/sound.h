629427dc1a2d2c198294489a3d4820430a7f7dbb
xstarisborn <aidan15@naver.com>
2025-12-04 18:51:57 +0900

    Add sound.h for sound effect management functions

---
+++ src/sound.h
@@ -0,0 +1,16 @@
+#ifndef SOUND_H
+#define SOUND_H
+
+#include "defs.h"
+
+// 효과음 초기화/종료 함수
+void InitSound(void);
+void CleanupSound(void);
+
+// 효과음 로딩 함수
+void LoadEffects(void);
+
+// 효과음 플레이 함수
+void PlaySlashSound(void);
+
+#endif
