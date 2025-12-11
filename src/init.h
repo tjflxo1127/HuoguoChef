5b0a567b4fdac3b98282f42af0bee246d4bcae86
xstarisborn <aidan15@naver.com>
2025-12-11 19:52:09 +0900

    Update init.h

--- src/init.h
+++ src/init.h
@@ -12,12 +12,29 @@
 #include "draw.h"
 #include "input.h"
 
-// 초기화 및 정리 함수
+/**
+ * @brief SDL, 리소스, 게임 객체를 초기화한다.
+ */
 void init_sdl(void);
 
+/**
+ * @brief UI 텍스트 객체를 초기화하고 배치한다.
+ */
 void InitUI();
+
+/**
+ * @brief 재료 이미지와 관련 리소스를 로드한다.
+ */
 void InitIngredient(void);
+
+/**
+ * @brief 마우스 궤적 버퍼를 초기화한다.
+ */
 void InitTrail(void);
+
+/**
+ * @brief 모든 리소스를 해제하고 SDL을 종료한다.
+ */
 void cleanup_sdl(void);
 
 #endif
