891b9dd40d9f1a877c6bf0a56e0227c1cdd695f0
shynn2 <zkxkflsk427a@gmail.com>
2025-12-13 17:13:23 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -9,19 +9,19 @@ int main(int argc, char *argv[]) {
     // 랜덤 시드 설정
     srand((unsigned int)time(NULL));
 
-    // 1. 초기화
+    // 초기화
     init_sdl();
 
      // BGM 재생 시작
     PlayBGM();
 
-    // [추가] 게임 상태 초기화
+    // 게임 상태 초기화
     app.game.score = 0;
     app.game.lives = 3;
     app.game.game_over = 0;
     app.game.title_screen = 1;  // 타이틀 화면으로 시작
 
-    // 2. 게임 루프
+    // 게임 루프
     while (g_game_running) {
         handle_events(); 
         update_game(); 
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
         SDL_Delay(1000 / 60); 
     }
 
-    // 3. 종료
+    // 종료
     cleanup_sdl();
     return 0;
 }
