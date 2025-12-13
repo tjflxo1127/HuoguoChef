411760b7541d12ac4ced0ff185294413ce0db97b
shynn2 <zkxkflsk427a@gmail.com>
2025-12-13 16:21:41 +0900

    Update utils.c

--- src/utils.c
+++ src/utils.c
@@ -6,10 +6,9 @@
 #include "utils.h"
 
 // 선분(궤적)과 사각형(재료)의 교차 판정
-// SDL_IntersectRectAndLine 함수를 사용하여 정밀하게 판정합니다.
+// SDL_IntersectRectAndLine 함수를 사용하여 판정
 int CheckLineRectHit(int x1, int y1, int x2, int y2, const SDL_Rect *rect) {
-    // SDL_IntersectRectAndLine 함수는 원본 좌표 변수를 수정하여 
-    // '잘린 선분'을 반환하므로, 원본 좌표를 보호하기 위해 임시 변수를 사용합니다.
+    // SDL_IntersectRectAndLine 함수는 원본 좌표 변수를 수정하여 잘린 선분을 반환하므로, 원본 좌표를 보호하기 위해 임시 변수 사용
     int tx1 = x1, ty1 = y1;
     int tx2 = x2, ty2 = y2;
 
